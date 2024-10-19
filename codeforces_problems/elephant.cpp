971592a2912b14cabff5bdf75be495d968f06e37
Mr-Unknown726 <115414319+Mr-Unknown726@users.noreply.github.com>
2024-10-19 13:08:46 -0400

    Create elephant.cpp

---
+++ codeforces_problems/elephant.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/contest/617/problem/A
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+
+    int ans = x/5 + (x%5!=0);
+    cout<<ans<<endl;
+
+    return 0;
+
+
+
+
+
+
+    
+}
