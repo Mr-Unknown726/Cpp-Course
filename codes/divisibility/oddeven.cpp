3190de375899fb3aba2ce8424b0dc49641f5c465
Mr-Unknown726 <115414319+Mr-Unknown726@users.noreply.github.com>
2024-11-03 10:16:45 -0500

    Create oddeven.cpp

---
+++ codes/divisibility/oddeven.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;  //declaring integer variable n
+
+    cin>>n;  // taking input into n
+
+    if (n%2==0){  //single = for assignmnet and double == for comparison
+        cout<<"the number is even"<<endl;
+    }
+
+    else {
+        cout<<"the number is odd"<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
