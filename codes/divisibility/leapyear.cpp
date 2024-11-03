66bc841a0fbb0c2cc3fe313f97c6dc6ef954fec5
Mr-Unknown726 <115414319+Mr-Unknown726@users.noreply.github.com>
2024-11-03 10:48:21 -0500

    Create leapyear.cpp

---
+++ codes/divisibility/leapyear.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int y; //declaring integer variable y
+
+    cin>>y; //taking input into y
+
+    if (y%4==0){
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
+    }
+
+    return 0;
+}
