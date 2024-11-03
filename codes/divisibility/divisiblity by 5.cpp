9a9700617956253bea03992e4e01c06f35a1888d
Mr-Unknown726 <115414319+Mr-Unknown726@users.noreply.github.com>
2024-11-03 10:43:23 -0500

    Create divisiblity by 5.cpp

---
+++ codes/divisibility/divisiblity by 5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int n; //declaring integer variable n
+
+    cin>>n; //taking input into n
+
+    if (n%5==0){
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"no"<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
